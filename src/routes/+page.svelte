6ad08a15b491ac65a3520cd52fb6504a520c6289
BaranSh28 <158576430+BaranSh28@users.noreply.github.com>
2025-05-10 18:56:12 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,5 @@
   // optional: you can redirect to login here if you want
 </script>
 
-<h1>Welcome to the Demens Check-in App</h1>
+<h1>Velkomm til Ari's Demens Check-in App</h1>
 <p><a href="/login">Go to Login</a></p>
