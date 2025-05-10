54eaff26733fbdfd23a593608847dd02164c75ba
BaranSh28 <158576430+BaranSh28@users.noreply.github.com>
2025-05-10 15:08:24 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/node';
+import { vitePreprocess } from 'svelte-preprocess';
 
 export default {
   kit: {
